cb112547eb527ded9184d8b95aa20aabd0ba2bbe
Migheli <avanesyan.migel@gmail.com>
2022-08-27 09:53:42 +0300

    Create and register newsparser app

--- Parser/settings.py
+++ Parser/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newsparser.apps.NewsparserConfig',
 ]
 
 MIDDLEWARE = [
---
+++ newsparser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewsparserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'newsparser'
