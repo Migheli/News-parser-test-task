ef8e90a69482584fced2e69a42b49f398ec43b35
Migheli <avanesyan.migel@gmail.com>
2022-08-30 23:27:12 +0300

    Optimized views.py

--- newsparser/views.py
+++ newsparser/views.py
@@ -1,52 +1,29 @@
 from django.http import JsonResponse
 from newsparser.models import Article, Tag
-# Create your views here.
 
 
-def show_news_by_tagname(request, hashtag):
-    target_articles = Article.objects.filter(tags__title__contains=hashtag)
-    serialized_articles = []
-    for article in target_articles:
-        tags_data=[]
-        if article.tags:
-            tags = Tag.objects.filter(articles=article)
-            for tag in tags:
-                tags_data.append(tag.title)
-
-
-        serialized_article = {
+def get_serialized_dataset(articles):
+    serialized_articles = [
+        {
             "title": article.title,
             "published_at": article.published_at,
             "channel": article.channel.title,
             "text": article.text,
-            "tags": tags_data
+            "tags": [tag.title for tag in Tag.objects.filter(articles=article) if
+                     article.tags]
         }
-        serialized_articles.append(serialized_article)
-    serialized_dataset = {'related_news': [serialized_articles]}
+        for article in articles
+    ]
+    return {'related_news': [serialized_articles]}
 
+
+def show_news_by_tagname(request, hashtag):
+    target_articles = Article.objects.filter(tags__title__contains=hashtag)
+    serialized_dataset = get_serialized_dataset(target_articles)
     return JsonResponse(serialized_dataset, json_dumps_params={'ensure_ascii': False})
 
 
 def show_all_news(request):
     articles = Article.objects.all().order_by('-published_at')
-
-    serialized_articles = []
-    for article in articles:
-        tags_data = []
-        if article.tags:
-            tags = Tag.objects.filter(articles=article)
-            for tag in tags:
-                tags_data.append(tag.title)
-
-
-        serialized_article = {
-            "title": article.title,
-            "published_at": article.published_at,
-            "channel": article.channel.title,
-            "text": article.text,
-            "tags": tags_data
-        }
-        serialized_articles.append(serialized_article)
-    serialized_dataset = {'related_news': [serialized_articles]}
-
+    serialized_dataset = get_serialized_dataset(articles)
     return JsonResponse(serialized_dataset, json_dumps_params={'ensure_ascii': False})
